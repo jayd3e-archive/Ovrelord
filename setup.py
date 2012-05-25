4701cede68b96c53911c3a8a1715928d29ec341e
Joe Dallago <joed@surveymonkey.com>
2012-05-25 14:29:29 -0700

    Base commit of necessary files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+requires = [
+    'retools'
+    ]
+
+setup(name='overlord',
+      version='0.0',
+      description='Overlord',
+      packages=['overlord'],
+      install_requires=requires,
+      )
